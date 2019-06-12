71f8e7e1b1ee9a27a93d33a5c09a875cf8e08550
Alex Frankenberger <alex@edv-frankenberger.de>
2019-06-12 10:12:13 +0200

    changed card content

--- bin/card.js
+++ bin/card.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-// 👆 Used to tell Node.js that this is a CLI tool
+// ðŸ‘† Used to tell Node.js that this is a CLI tool
 
 // Pull in our modules
 const chalk = require('chalk');
 const boxen = require('boxen');
+
 
 // Define options for Boxen
 const options = {
@@ -13,16 +14,15 @@ const options = {
 };
 // Text + chalk definitions
 const data = {
-  'name': chalk.bold.white('Dominik Lindemann /'),
-  'handle': chalk.cyan('dominiklindemann'),
-  'work': chalk.blueBright('Founder & CTO @ MunichConsultants'),
-  'twitter': chalk.cyan('https://twitter.com/DomLindemann'),
-  'github': chalk.cyan('https://github.com/dominiklindemann'),
-  'linkedin': chalk.cyan('https://linkedin.com/in/dominik-lindemann'),
-  'xing': chalk.cyan('https://www.xing.com/profile/Dominik_Lindemann'),
-  'web': chalk.cyan('http://d-lindemann.de'),
-  'webMuc': chalk.cyan('http://munichConsultants.tech'),
-  'npx': chalk.white('npx dominiklindemann'),
+  'name': chalk.white('Alex Frankenberger/'),
+  'handle': chalk.cyan('alexfrankenberger'),
+  'work': chalk.blueBright('Founder & CEO @ MunichConsultants'),
+  'twitter': chalk.cyan('@AlexFrankenberg'),
+  'github': chalk.cyan('https://github.com/alexfrankenberger'),
+  'linkedin': chalk.cyan('https://www.linkedin.com/in/alex-frankenberger-929a28159/'),
+  'xing': chalk.cyan('https://www.xing.com/profile/Alex_Frankenberger'),
+  'web': chalk.cyan('http://munichconsultants.tech'),
+  'npx': chalk.white('npx alexfrankenberger'),
   'labelWork': chalk.white.bold('      Work:'),
   'labelTwitter': chalk.white.bold('   Twitter:'),
   'labelGitHub': chalk.white.bold('    GitHub:'),
@@ -34,18 +34,16 @@ const data = {
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} ${data.handle}`;
-const working = `${data.labelWork}  ${data.work}`;
-const twittering = `${data.labelTwitter}  ${data.twitter}`;
-const githubing = `${data.labelGitHub}  ${data.github}`;
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
-const xinging = `${data.labelXing}  ${data.xing}`;
-const webing = `${data.labelWeb}  ${data.web}`;
-const webingMuc = `${data.labelWeb}  ${data.webMuc}`;
-const carding = `${data.labelCard}  ${data.npx}`;
+const heading = ${data.name} ${data.handle};
+const working = ${data.labelWork}  ${data.work};
+const twittering = ${data.labelTwitter}  ${data.twitter};
+const githubing = ${data.labelGitHub}  ${data.github};
+const linkedining = ${data.labelLinkedIn}  ${data.linkedin};
+const xinging = ${data.labelXing}  ${data.xing};
+const webing = ${data.labelWeb}  ${data.web};
+const carding = ${data.labelCard}  ${data.npx};
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + newline + twittering + newline + githubing + newline + linkedining + newline + xinging + newline + webing + newline + webingMuc + newline + newline + carding;
+const output = heading + newline + newline + working + newline + newline + twittering + newline + githubing + newline + linkedining + newline + xinging + newline + webing + newline + newline + carding;
 
 console.log(chalk.white(boxen(output, options)));
-
