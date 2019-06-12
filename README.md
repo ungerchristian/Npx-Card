65ebe817db2313cc23bfc39615a31a9880349686
Christian Unger <christian@munichconsultants.tech>
2019-06-12 14:27:12 +0200

    First version of business card

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-This is Dominik!
+This is Christian!
 
 # Usage
 Via npx:
 ```
-npx dominiklindemnn
+npx chritsianunger
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- bin/card.js
+++ bin/card.js
@@ -14,19 +14,19 @@ const options = {
 };
 // Text + chalk definitions
 const data = {
-  'name': chalk.white('Alex Frankenberger/'),
-  'handle': chalk.cyan('alexfrankenberger'),
-  'work': chalk.blueBright('Founder & CEO @ MunichConsultants'),
-  'twitter': chalk.cyan('@AlexFrankenberg'),
-  'github': chalk.cyan('https://github.com/alexfrankenberger'),
-  'linkedin': chalk.cyan('https://www.linkedin.com/in/alex-frankenberger-929a28159/'),
-  'xing': chalk.cyan('https://www.xing.com/profile/Alex_Frankenberger'),
+  'name': chalk.white('Christian Unger'),
+  'handle': chalk.cyan('christianunger'),
+  'work': chalk.blueBright('CIO @ MunichConsultants'),
+  //'twitter': chalk.cyan('@AlexFrankenberg'),
+  'github': chalk.cyan('https://github.com/ungerchristian'),
+  //'linkedin': chalk.cyan('https://www.linkedin.com/in/alex-frankenberger-929a28159/'),
+  'xing': chalk.cyan('https://www.xing.com/profile/Christian_Unger32'),
   'web': chalk.cyan('http://munichconsultants.tech'),
-  'npx': chalk.white('npx alexfrankenberger'),
+  'npx': chalk.white('npx christianunger'),
   'labelWork': chalk.white.bold('      Work:'),
-  'labelTwitter': chalk.white.bold('   Twitter:'),
+  //'labelTwitter': chalk.white.bold('   Twitter:'),
   'labelGitHub': chalk.white.bold('    GitHub:'),
-  'labelLinkedIn': chalk.white.bold('  LinkedIn:'),
+  //'labelLinkedIn': chalk.white.bold('  LinkedIn:'),
   'labelXing': chalk.white.bold('      Xing:'),
   'labelWeb': chalk.white.bold('       Web:'),
   'labelCard': chalk.white.bold('      Card:')
@@ -34,16 +34,16 @@ const data = {
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = ${data.name} ${data.handle};
-const working = ${data.labelWork}  ${data.work};
-const twittering = ${data.labelTwitter}  ${data.twitter};
-const githubing = ${data.labelGitHub}  ${data.github};
-const linkedining = ${data.labelLinkedIn}  ${data.linkedin};
-const xinging = ${data.labelXing}  ${data.xing};
-const webing = ${data.labelWeb}  ${data.web};
-const carding = ${data.labelCard}  ${data.npx};
+const heading = `${data.name} / ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+//const twittering = "${data.labelTwitter}  ${data.twitter}";
+const githubing = `${data.labelGitHub}  ${data.github}`;
+//const linkedining = "${data.labelLinkedIn}  ${data.linkedin};
+const xinging = `${data.labelXing}  ${data.xing}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + newline + twittering + newline + githubing + newline + linkedining + newline + xinging + newline + webing + newline + newline + carding;
+const output = heading + newline + newline + working + newline + newline + githubing + newline + xinging + newline + webing + newline + newline + carding;
 
 console.log(chalk.white(boxen(output, options)));
